0aa3c60a63dd3e7e7e2e6ede7480bb91aa210a98
Zhao Tianyu <davidxk@qq.com>
2016-04-06 21:19:11 +0800

    SaveAction Vec version for real

--- backend/mafenghive/src/beehive/controller/SaveAction.java
+++ backend/mafenghive/src/beehive/controller/SaveAction.java
@@ -2,6 +2,7 @@ package beehive.controller;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Vector;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -39,25 +40,24 @@ public class SaveAction extends HttpServlet {
 		}
 		
 		// Read report data from <form>
-		final int key_len = 7;
 		String keys[] = { "pho", "tim", "co1", "tem", "hum", "noi", "ult" };
-		String[] values = new String[key_len];
-		for(int i=0; i<key_len; i++)
-			values[i] = request.getParameter(keys[i]);
+		Vector<String> values = new Vector<String>();
+		for(String key: keys)
+			values.add( request.getParameter(key) );
 		
 		// If user phone not exist, return
-		int phone = Integer.parseInt(values[0]);
+		int phone = Integer.parseInt(values.get(0));
 		//if(!UserDAO.has(phone))
 			//return;
 
 		// Initiate a Report object
 		Date timestamp = new Date();
 		Report report = new Report(phone, timestamp,
-				Float.parseFloat(values[2]),
-				Float.parseFloat(values[3]),
-				Float.parseFloat(values[4]),
-				Float.parseFloat(values[5]),
-				Float.parseFloat(values[6])
+				Float.parseFloat(values.get(2)),
+				Float.parseFloat(values.get(3)),
+				Float.parseFloat(values.get(4)),
+				Float.parseFloat(values.get(5)),
+				Float.parseFloat(values.get(6))
 				);
 
 		// Call ReportDao to save the object
